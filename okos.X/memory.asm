5d2f0fc842bcc723b2cb0b1831b841cff42a3163
Ben Hencke <brainstar@gmail.com>
2016-11-27 17:33:52 -0800

    memory

--- okos.X/memory.asm
+++ okos.X/memory.asm
@@ -1,14 +1,13 @@
     udata
-line		res 32
-bitbucket	res 1 ; a memory location just before buffer, allows underruning by 1 to save code space
-buffer		res 1 ; the rest of memory
 
     udata_acs
     
 flags		res 1; flag bit register
-	
-tableSearch	res 1
-tableRes	res 1
+
+currentFile	res 1; which bank of memory is active
+
+;tableSearch	res 1
+;tableRes	res 1
 keyboardCode	res 1
 keyboardAscii	res 1
 
@@ -18,18 +17,19 @@ oledCol		res 1
 oledSegment	res 1
 oledFontData	res 2
 	
-editorTemp	res 1
+;editorTemp	res 1
 	
 cursorX		res 1
 cursorY		res 1
 
-parsedWord1	res 1
-parsedWord2	res 2
-parsedWord3	res 1
+;parsedWord1	res 1
+;parsedWord2	res 2
+;parsedWord3	res 1
 	
 stringsTemp	res 1
-
-#define BUFFER_MAX_ADDR (buffer+1900)
+	
+bitbucket	res 1 ; a memory location just before buffer, allows underruning by 1 to save code space
+buffer		res 1 ; the rest of memory
 
 #define keyboardIgnore flags,1,access
 #define oledDrawCursor flags,2,access
